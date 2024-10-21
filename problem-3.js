f34a89e1efeabae2bf7b1d3d31abe10d9841cfd4
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-10-21 21:18:42 +0600

    problem-3 done

---
+++ problem-3.js
@@ -0,0 +1,44 @@
+// the number small to big count
+const roolNumber = [
+  "3",
+  "67",
+  "12",
+  "45",
+  "100",
+  "109",
+  "98",
+  "1",
+  "2",
+  "6",
+  "86",
+];
+
+const rolls = roolNumber.sort(function (a, b) {
+  return a - b;
+});
+
+console.log(rolls);
+
+// the number bim small
+
+const employ = [
+  "3",
+  "67",
+  "12",
+  "45",
+  "100",
+  "109",
+  "65",
+  "85",
+  "98",
+  "1",
+  "2",
+  "6",
+  "86",
+];
+
+const employers = employ.sort(function (a, b) {
+  return b - a;
+});
+
+console.log(employers);
