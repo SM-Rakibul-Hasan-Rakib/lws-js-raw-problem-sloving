9b6ee48020750d16a2ddf4bcaf6039f94f2c5515
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-10-22 15:23:44 +0600

    6 problem updated

---
+++ problem-6.js
@@ -0,0 +1,11 @@
+// const numbers = [1, 2, 3, 4, 67, 834, 45, 3435, 3244, 35, 6, 7, 8, 23, 34];
+
+// const duplicats = numbers.filter(function (value, index, array) {
+//   return array.indexOf(value) !== value;
+// });
+// console.log(duplicats);
+function add(a, b) {
+  // 'a' and 'b' are parameters
+  return a + b;
+}
+console.log(add(5, 10));
