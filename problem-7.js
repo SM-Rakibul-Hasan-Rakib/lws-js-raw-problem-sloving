d8bbacad4aac31b355bf5f14043f700a31e47977
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-10-24 21:33:10 +0600

    problem-7 parties

---
+++ problem-7.js
@@ -0,0 +1,20 @@
+let age = 6;
+let boyosh = 3;
+if (age < 10 && boyosh > 1) {
+  console.log("true");
+} else {
+  console.log("flase");
+}
+
+let rakib = 18;
+let rafi = 22;
+if (rakib < 22 && rafi > 18) {
+  console.log("Rakib r thika Rafi boro");
+} else {
+  console.log("Rafi thika Rakib boro");
+}
+// if ((age) => 19) {
+//   console.log("you are adult");
+// } else {
+//   ("you are not adult");
+// }
