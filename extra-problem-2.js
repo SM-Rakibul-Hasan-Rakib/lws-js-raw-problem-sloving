4b829dd6c4336d92389b75a36b457cd433e3d6d2
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-10-27 16:06:42 +0600

    problem sloved

---
+++ extra-problem-2.js
@@ -0,0 +1,26 @@
+// check if the number is even or odd
+// macknisom
+const num = 6;
+function checkEvenOdd(x) {
+  if (x % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+const result = checkEvenOdd(num);
+console.log(result);
+
+// normal funcation declear
+
+const number = 5;
+
+function finalNumber(x) {
+  if (5 % 2 === 0) {
+    console.log("Even");
+  } else {
+    console.log("Odd");
+  }
+}
+finalNumber(number);
