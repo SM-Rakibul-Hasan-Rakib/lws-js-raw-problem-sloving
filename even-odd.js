e1dde73a13a8b7605d0b1b9ebd8824de163e3cf1
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-12-12 23:46:46 +0600

    even odd done

---
+++ even-odd.js
@@ -0,0 +1,19 @@
+// check if the number or odd
+
+function EvenOdd(x){
+  
+if(x % 2 === 0){
+  console.log("even");
+}  else{
+  console.log("Odd");
+}
+}
+EvenOdd(6) 
+
+function printName(x){
+  for(let i = 1; i <= x ; i++){
+    console.log("first");
+  }
+}
+
+printName(3)
