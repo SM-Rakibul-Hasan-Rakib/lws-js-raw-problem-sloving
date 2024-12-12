db3bf53081e7a3b5ddc6b09b037efb0850f3ea10
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-12-12 21:34:03 +0600

    problem.js added

---
+++ problem.js
@@ -0,0 +1,11 @@
+function printName(name) {
+  console.log("my name is", name);
+}
+
+printName("Rakib Khan");
+
+function printDetal(name, age, addess) {
+  console.log(`My name is ${name}.My age is ${age}.My address ${addess}`);
+}
+
+printDetal("Rakib", "18", "Dhaka");
