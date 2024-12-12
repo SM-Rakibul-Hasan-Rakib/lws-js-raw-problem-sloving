9bb86f073116c8d13b152013b0e400b376fe113a
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-12-12 21:54:01 +0600

    funcation main concep clear

---
+++ funcationTypes.js
@@ -0,0 +1,30 @@
+// types of funcation
+
+// no parameters, not return;
+// no Parameters, but return;
+// parameters , but not return;
+// parameters ,return
+
+// no parameters , not return;
+
+// no parameters, not return;
+function fun1(){
+  console.log("no parameters, no return");
+}
+
+// no Parameters, but return;
+function fun2 (){
+  console.log("no parameter, no return");
+   return 5;
+}
+
+// parameters , but not return;
+function fun3(name){
+  console.log(name);
+}
+
+
+// parameters ,return
+function fund(x,y){
+  return x + y;
+}
